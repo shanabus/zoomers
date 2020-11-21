b5ea9670ee23db6fcd2619e5492b1dad8c4969fb
Shane Sievers <shanabus@gmail.com>
2020-11-20 22:51:05 -0500

    fix lobby icons

--- Server/Hubs/GameHub.cs
+++ Server/Hubs/GameHub.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
 using ZoomersClient.Shared.Models;
 using ZoomersClient.Shared.Models.Enums;
 using ZoomersClient.Shared.Services;
@@ -12,13 +13,15 @@ namespace ZoomersClient.Server.Hubs
     {
         // for background service cross-hub comms, check out https://docs.microsoft.com/en-us/aspnet/core/signalr/background-services?view=aspnetcore-5.0
 
+        private ILogger<GameService> _logger { get; set;}
         public List<string> Players { get; set; }
         public GameService _gameService { get; set;}        
 
-        public GameHub(GameService gameService)
+        public GameHub(ILogger<GameService> logger, GameService gameService)
         {
             Players = new List<string>();
             _gameService = gameService;
+            _logger = logger;
         }
 
         public async Task JoinGame(string user)
@@ -30,7 +33,7 @@ namespace ZoomersClient.Server.Hubs
             await Clients.All.SendAsync("PlayerJoined", Players);
         }
 
-        public async Task JoinTheGame(List<PartyIcon> partyIcons, string username)
+        public async Task JoinTheGame(List<PartyIcon> partyIcons, string username, PlayerIcon icon, string color)
         {
             var game = _gameService.FindGame(partyIcons);
             if (game == null)
@@ -41,7 +44,14 @@ namespace ZoomersClient.Server.Hubs
             {                
                 await Clients.Caller.SendAsync("PlayerJoined", game);
                 
-                var player = new Player() { ConnId = Context.ConnectionId, Username = username };
+                var player = new Player() { 
+                    ConnId = Context.ConnectionId, 
+                    Username = username,
+                    Icon = icon,
+                    BackgroundColor = color    
+                };
+                _logger.LogInformation(player.Username + " connected");
+
                 var updatedGame = _gameService.JoinGame(game.Id, player);
 
                 //await Clients.All.SendAsync("PlayersUpdated", updatedGame);

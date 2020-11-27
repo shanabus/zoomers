d77044864972b16f604ae07c00edc734f5107680
Shane Sievers <shanabus@gmail.com>
2020-11-27 16:52:53 -0500

    final prep before overhaul

---
+++ Shared/Models/Enums/PlayState.cs
@@ -0,0 +1,11 @@
+namespace ZoomersClient.Shared.Models.Enums
+{    
+    public enum PlayState
+    {
+        AnsweringQuestion,
+        AnswerSubmitted,
+        CurrentPlayer,
+        CurrentPlayerAnswering,
+        QuestionScores
+    }
+}
